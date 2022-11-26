1f1811ddac2c6ec32a91580fddf14d2a93b80629
thinh tu <95674091+tkthinh@users.noreply.github.com>
2022-11-26 17:41:54 +0700

    styling gio hang

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,182 +1,170 @@
-const cartIcon = document.querySelector(".cart-icon")
-const cartClose = document.querySelector(".close-cart")
-const cartclear = document.querySelector(".clear-cart")
-const cartDOM = document.querySelector(".cart")
-const cartOverlay = document.querySelector(".cart-overlay")
-const cartCounter =document.querySelector(".cart-counter")
-const cartTotal =document.querySelector(".cart-total")
-const cartContent =document.querySelector(".cart-content")
-const cartItems = document.querySelector(".cart-counter")
+const cartIcon = document.querySelector(".cart-icon");
+const cartClose = document.querySelector(".close-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartCounter = document.querySelector(".cart-counter");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const cartItems = document.querySelector(".cart-counter");
 
-let cart=[];
-let buttonsDOM=[]
-    getbuttons=(incart)=>{
-    const buttons =[...document.querySelectorAll(".buy-btn")]
-    buttonsDOM =buttons
-    buttons.forEach(button => {
-        let id = button.dataset.id;
-        let inCart =incart.find(item => item.id == id);
-        if(inCart){
-            button.innerText ="Đã thêm vào giỏ";
-            button.disabled = true;
-        }
-            button.addEventListener("click",event =>{
-                    event.target.innerText="Đã thêm vào giỏ";
-                    event.target.disabled =true;
-                    let cartItem = {...getProduct(id), amount:1}
-                    console.log(cart)
-                    incart.push(cartItem)
-                    // [...cart,cartItem]
-                    console.log(cart)
-                    saveCart(cart) // save vào local
-                    setCartValue(cart)
-                    addCartItem(cartItem)           
-            })  
+let cart = [];
+let buttonsDOM = [];
+getbuttons = (incart) => {
+  const buttons = [...document.querySelectorAll(".buy-btn")];
+  buttonsDOM = buttons;
+  buttons.forEach((button) => {
+    let id = button.dataset.id;
+    let inCart = incart.find((item) => item.id == id);
+    if (inCart) {
+      button.innerText = "Đã thêm vào giỏ";
+      button.disabled = true;
+    }
+    button.addEventListener("click", (event) => {
+      event.target.innerText = "Đã thêm vào giỏ";
+      event.target.disabled = true;
+      let cartItem = { ...getProduct(id), amount: 1 };
+      console.log(cart);
+      incart.push(cartItem);
+      // [...cart,cartItem]
+      console.log(cart);
+      saveCart(cart); // save vào local
+      setCartValue(cart);
+      addCartItem(cartItem);
     });
-}
-    
-     getProduct=(id)=>{
-        let products =JSON.parse(window.localStorage.getItem('product')).book;
-        return products.find(ele=>{return ele.id ==id})
-    };
-    saveCart=(cart)=>{
-        localStorage.setItem("cart", JSON.stringify(cart))
-    }
-    setCartValue=(cart)=>{
-        let tempTotal =0
-        let itemsTotal =0
-        cart.map(item =>{
-            tempTotal += item.price * item.amount
-            itemsTotal +=item.amount
-        })
-        cartTotal.innerHTML = parseFloat(tempTotal.toFixed(2))
-        cartItems.innerHTML = itemsTotal
-        console.log(cartTotal, cartItems)
-    }
-    addCartItem=(item)=>{
-        const div = document.createElement('div')
-        div.classList.add('cart-item')
-            div.innerHTML=`<img src="`+item.image+`" alt="">
+  });
+};
+
+getProduct = (id) => {
+  let products = JSON.parse(window.localStorage.getItem("product")).book;
+  return products.find((ele) => {
+    return ele.id == id;
+  });
+};
+saveCart = (cart) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+setCartValue = (cart) => {
+  let tempTotal = 0;
+  let itemsTotal = 0;
+  cart.map((item) => {
+    tempTotal += item.price * item.amount;
+    itemsTotal += item.amount;
+  });
+  cartTotal.innerHTML = parseFloat(tempTotal.toFixed(2)) + '.000đ';
+  cartItems.innerHTML = itemsTotal;
+  console.log(cartTotal, cartItems);
+};
+addCartItem = (item) => {
+  const div = document.createElement("div");
+  div.classList.add("cart-item");
+  div.innerHTML=`<img src="`+item.image+`" alt="">
             <div>
               <h4>`+item.title+`</h4>
               <h5>`+item.price+`.000đ</h5>
-              <span class="remove-item" data-id="`+item.id +`">remove</span>
+              <span class="remove-item" data-id="`+item.id +`"><i class="fa-solid fa-trash-can"></i> Xoá</span>
             </div>
             <div>
               <i class="fas fa-chevron-up" data-id="`+item.id +`"></i>
               <p class="item-amount">`+item.amount+`</p>
               <i class="fas fa-chevron-down" data-id="`+item.id +`"></i>
           </div>`
-          cartContent.appendChild(div)
-          console.log(cartContent)
+  cartContent.appendChild(div);
+  console.log(cartContent);
+};
+showCart = () => {
+  cartOverlay.classList.add("transparentBcg");
+  cartDOM.classList.add("showCart");
+};
+hideCart = () => {
+  cartOverlay.classList.remove("transparentBcg");
+  cartDOM.classList.remove("showCart");
+};
+getCart = () => {
+  return localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
+};
+populate = (cart) => {
+  cart.forEach((item) => addCartItem(item));
+};
+setupAPP = (cart) => {
+  cart.splice(0, cart.length);
+  cart.push(...getCart());
+  setCartValue(cart);
+  populate(cart);
+  cartIcon.addEventListener("click", function () {
+    showCart();
+  });
+  cartClose.addEventListener("click", function () {
+    hideCart();
+  });
+  return cart;
+};
+getSingleButton = (id) => {
+  return buttonsDOM.find((button) => button.dataset.id == id);
+};
+removeItem = (id) => {
+  cart.splice(0, cart.length);
+  setCartValue(cart);
+  saveCart(cart);
+  let button = getSingleButton(id);
+  button.disabled = false;
+  button.innerHTML = `<i class="fas fa-shopping-cart" ></i>
+        Thêm vào giỏ hàng`;
+};
+cartLogic = (cart) => {
+  cartContent.addEventListener("click", (event) => {
+    if (event.target.classList.contains("remove-item")) {
+      let removeItem = event.target;
+      let id = removeItem.dataset.id;
+      cartContent.removeChild(removeItem.parentElement.parentElement);
+      for (let i = cart.length - 1; i >= 0; --i)
+        if (cart[i].id == id) cart.splice(i, 1);
+      console.log(cart);
+      setCartValue(cart);
+      saveCart(cart);
+      let button = getSingleButton(id);
+      button.disabled = false;
+      button.innerHTML = `<i class="fas fa-shopping-cart" ></i>
+                Thêm vào giỏ hàng`;
+    } else if (event.target.classList.contains("fa-chevron-up")) {
+      let addAmount = event.target;
+      let id = addAmount.dataset.id;
+      let tempItem = cart.find((item) => item.id == id);
+      tempItem.amount = tempItem.amount + 1;
+      saveCart(cart);
+      setCartValue(cart);
+      addAmount.nextElementSibling.innerText = tempItem.amount;
+    } else if (event.target.classList.contains("fa-chevron-down")) {
+      let lowerAmount = event.target;
+      let id = lowerAmount.dataset.id;
+      let tempItem = cart.find((item) => item.id == id);
+      tempItem.amount = tempItem.amount - 1;
+      if (tempItem.amount > 0) {
+        saveCart(cart);
+        setCartValue(cart);
+        lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        console.log(tempItem.amount);
+      } else {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        for (let i = cart.length - 1; i >= 0; --i)
+          if (cart[i].id == id) cart.splice(i, 1);
+        console.log(cart);
+        setCartValue(cart);
+        saveCart(cart);
+        let button = getSingleButton(id);
+        button.disabled = false;
+        button.innerHTML = `<i class="fas fa-shopping-cart" ></i>
+                Thêm vào giỏ hàng`;
+      }
     }
-    showCart=()=>{
-        cartOverlay.classList.add("transparentBcg")
-        cartDOM.classList.add("showCart")
-    }
-    hideCart=()=>{
-        cartOverlay.classList.remove("transparentBcg")
-        cartDOM.classList.remove("showCart")
-    }
-    getCart=()=>{
-        return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[]
-    }
-    populate=(cart)=>{
-        cart.forEach(item => addCartItem(item))
-    }
-    setupAPP=(cart)=>{
-        cart.splice(0, cart.length );
-        cart.push(...getCart());
-        setCartValue(cart)
-        populate(cart)
-        cartIcon.addEventListener('click',function(){showCart()})
-        cartClose.addEventListener('click',function(){hideCart()})
-        return cart
-    }
-    getSingleButton=(id)=>{
-        return buttonsDOM.find( button => button.dataset.id == id)
-    } 
-    removeItem=(id)=>{
-        cart.splice(0,cart.length)
-        setCartValue(cart)
-        saveCart(cart)
-        let button = getSingleButton(id)
-        button.disabled = false
-        button.innerHTML=`<i class="fas fa-shopping-cart" ></i>
-        Thêm vào giỏ hàng`
-        
-    } 
-       clearCart=(cart)=>{
-        let cartItems= cart.map(item => item.id)
-        console.log(cartItems)
-        cartItems.forEach(id => removeItem(id))
-        console.log(cartContent.children)
-        while(cartContent.children.length>0){
-            cartContent.removeChild(cartContent.children[0])
-        }
-        hideCart()
-    }
-    cartLogic=(cart)=>{
-        cartclear.addEventListener("click",()=>{
-            clearCart(cart)
-        })
-        cartContent.addEventListener("click",event=>{
-            if(event.target.classList.contains("remove-item")){
-                let removeItem = event.target
-                let id= removeItem.dataset.id
-                cartContent.removeChild(removeItem.parentElement.parentElement)
-                for (let i = cart.length - 1; i >= 0; --i)
-                    if (cart[i].id == id) cart.splice(i, 1);
-                console.log(cart);
-                setCartValue(cart)
-                saveCart(cart)
-                let button = getSingleButton(id)
-                button.disabled = false
-                button.innerHTML=`<i class="fas fa-shopping-cart" ></i>
-                Thêm vào giỏ hàng`
-            }
-            else if(event.target.classList.contains("fa-chevron-up")){
-                let addAmount =event.target
-                let id = addAmount.dataset.id
-                let tempItem= cart.find(item => item.id == id)
-                tempItem.amount = tempItem.amount +1
-                saveCart(cart)
-                setCartValue(cart)
-                addAmount.nextElementSibling.innerText=tempItem.amount
-            }
-            else if(event.target.classList.contains("fa-chevron-down")){
-                let lowerAmount =event.target
-                let id = lowerAmount.dataset.id
-                let tempItem= cart.find(item => item.id == id)
-                tempItem.amount = tempItem.amount -1
-                if(tempItem.amount>0){
-                    saveCart(cart)
-                    setCartValue(cart)
-                    lowerAmount.previousElementSibling.innerText=tempItem.amount
-                    console.log(tempItem.amount)
-                }else{
-                 
-                let removeItem = event.target
-                let id= removeItem.dataset.id
-                cartContent.removeChild(removeItem.parentElement.parentElement)
-                for (let i = cart.length - 1; i >= 0; --i)
-                    if (cart[i].id == id) cart.splice(i, 1);
-                console.log(cart);
-                setCartValue(cart)
-                saveCart(cart)
-                let button = getSingleButton(id)
-                button.disabled = false
-                button.innerHTML=`<i class="fas fa-shopping-cart" ></i>
-                Thêm vào giỏ hàng`
-                }
-            }
-        })
-    }
-      
-    document.addEventListener("DOMContentLoaded", ()=>{
-        cart=setupAPP(cart)
-        cartLogic(cart)
-        getbuttons(cart)
-    })
+  });
+};
 
-   
+document.addEventListener("DOMContentLoaded", () => {
+  cart = setupAPP(cart);
+  cartLogic(cart);
+  getbuttons(cart);
+});
